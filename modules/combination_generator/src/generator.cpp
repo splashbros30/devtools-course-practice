dd21e2fdba33965fa43950c975548b08f5311178
Denis <den_nikolaev_2000@mail.ru>
2021-04-26 16:39:50 +0300

    fifth

--- modules/combination_generator/src/generator.cpp
+++ modules/combination_generator/src/generator.cpp
@@ -9,8 +9,6 @@ bool generator::NextSet(int** a, int n, int m) {
     int j = m - 1;
     while ((*a)[j] == n && j >= 0) j--;
     if (j < 0) return false;
-    if ((*a)[j] >= n)
-        j--;
     (*a)[j]++;
     if (j == m - 1) return true;
     for (int k = j + 1; k < m; k++)
