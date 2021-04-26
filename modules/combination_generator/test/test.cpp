0c976010a28ec3f9f4c21051924cb3823a456fe4
Denis <den_nikolaev_2000@mail.ru>
2021-04-26 14:58:32 +0300

    first

---
+++ modules/combination_generator/test/test.cpp
@@ -0,0 +1,104 @@
+// Copyright 2021 Nikolaev Denis
+#include <gtest/gtest.h>
+#include "include/generator.h"
+
+TEST(Combination_Generate, Test1) {
+    int n = 5;
+    int m = 2;
+    generator g;
+    int h = n > m ? n : m;
+    int* a = new int[h];
+    for (int i = 0; i < h; i++)
+        a[i] = 1;
+
+    for (int i = 0; i < 5; i++) {
+        g.NextSet(&a, n, m);
+    }
+
+
+    int res[] = { 2, 2 };
+    for (int i = 0; i < m; i++) {
+        ASSERT_EQ(a[i], res[i]);
+    }
+}
+
+TEST(Combination_Generate, Test2) {
+    int n = 7;
+    int m = 3;
+    generator g;
+    int h = n > m ? n : m;
+    int* a = new int[h];
+    for (int i = 0; i < h; i++)
+        a[i] = 1;
+
+    for (int i = 0; i < 8; i++) {
+        g.NextSet(&a, n, m);
+    }
+
+
+    int res[] = { 1, 2, 3 };
+    for (int i = 0; i < m; i++) {
+        ASSERT_EQ(a[i], res[i]);
+    }
+}
+
+TEST(Combination_Generate, Test3) {
+    int n = 15;
+    int m = 4;
+    generator g;
+    int h = n > m ? n : m;
+    int* a = new int[h];
+    for (int i = 0; i < h; i++)
+        a[i] = 1;
+
+    for (int i = 0; i < 15; i++) {
+        g.NextSet(&a, n, m);
+    }
+
+
+    int res[] = { 1, 1, 2, 2 };
+    for (int i = 0; i < m; i++) {
+        ASSERT_EQ(a[i], res[i]);
+    }
+}
+
+TEST(Combination_Generate, Test4) {
+    int n = 100;
+    int m = 5;
+    generator g;
+    int h = n > m ? n : m;
+    int* a = new int[h];
+    for (int i = 0; i < h; i++)
+        a[i] = 1;
+
+    for (int i = 0; i < 58; i++) {
+        g.NextSet(&a, n, m);
+    }
+
+
+    int res[] = { 1, 1, 1, 1, 59 };
+    for (int i = 0; i < m; i++) {
+        ASSERT_EQ(a[i], res[i]);
+    }
+}
+
+TEST(Combination_Generate, Test5) {
+    int n = 50;
+    int m = 2;
+    generator g;
+    int h = n > m ? n : m;
+    int* a = new int[h];
+    for (int i = 0; i < h; i++)
+        a[i] = 1;
+
+    for (int i = 0; i < 125; i++) {
+        g.NextSet(&a, n, m);
+    }
+
+
+    int res[] = { 3, 29 };
+    for (int i = 0; i < m; i++) {
+        ASSERT_EQ(a[i], res[i]);
+    }
+}
+
